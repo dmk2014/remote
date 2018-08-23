c76e95a027888572ff2c9073570e7d02fd26f24a
dmk2014 <6460241+dmk2014@users.noreply.github.com>
2018-08-23 21:57:08 +0100

    Define config structure. Read config. Add sample config.

--- main.go
+++ main.go
@@ -1,14 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 )
 
-func main() {
+type Config struct {
+	Commands []Command
+	Chains   []Chain
+}
 
+type Command struct {
+	Name string
+	Path string
+	Args []string
+}
+
+type Chain struct {
+	Name    string
+	Command []Command
+}
+
+var config Config
+
+func main() {
+	readConfig()
+
+	startServer()
+}
+
+func readConfig() {
+	path := "remote.config.json"
+
+	file, err := os.Open(path)
+	if os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist at %s", path)
+	}
+	if err != nil {
+		log.Fatalf("Could not open config at %s\n%s", path, err.Error())
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	err = decoder.Decode(&config)
+	if err != nil && err != io.EOF {
+		log.Fatalf("Error parsing config at %s\n%s", path, err)
+	}
+}
+
+func startServer() {
 	address := "localhost:5000"
 
 	http.HandleFunc("/helloremote", func(w http.ResponseWriter, r *http.Request) {
